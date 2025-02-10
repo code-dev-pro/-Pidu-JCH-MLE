b1f94e6da489590d5102bf676717cf55db94edce
Julie-Charles16 <juliecharles2716@gmail.com>
2025-02-10 16:51:35 +0100

    feat(button): ajout d'un effet cliqué dans la console

--- src/components/my-button.tsx
+++ src/components/my-button.tsx
@@ -1,11 +1,18 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 export default function MyButton() {
   const [clicked, setClicked] = useState(false)
 
+  useEffect(() => {
+    if (clicked) {
+      console.log('Le bouton a été cliqué !')
+    }
+  }, [clicked])
+
   const handleClick = () => {
     setClicked(true)
   }
+
   const labelButton = clicked ? 'Clicked !' : 'Click !'
   return <button onClick={handleClick}>{labelButton}</button>
 }
