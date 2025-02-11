75ef38c6e75c2d5d8e3d48a985186ad47c70c78e
Marylinelesaffre <marylineetnoe@outlook.fr>
2025-02-11 11:49:48 +0100

    feat: useRef sur mon newbutton qui garde le nombre de clique

--- src/components/new-button.tsx
+++ src/components/new-button.tsx
@@ -1,10 +1,13 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 
 function ButtonSquare() {
   const [value, setValue] = useState('')
+  const ref = useRef(0)
 
   function handleClick() {
     setValue('X')
+    ref.current = ref.current + 1
+    console.log('Le bouton à était cliqué ' + ref.current + ' fois !')
   }
   useEffect(() => {
     if (value !== '') {
