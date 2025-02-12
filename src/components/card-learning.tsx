92cb986f4216e0999cd32865c2359c8d85db5b37
Julie Charles <126774803+Julie-Charles16@users.noreply.github.com>
2025-02-12 19:55:17 +0100

    feat: ajout d'une card Travel avec design rectangulaire, image réduite et centrée (#16)

---
+++ src/components/card-learning.tsx
@@ -0,0 +1,10 @@
+function CardTravel() {
+  return (
+    <div className="bg-white rounded-xl w-64 h-40 flex flex-col items-center justify-center p-4">
+      <img src="./Travel.svg" alt="Travel" className="w-16 h-16 object-contain mb-2" />
+      <h2 className="text-lg font-semibold text-gray-800 text-center">Travel</h2>
+    </div>
+  )
+}
+
+export default CardTravel
