b46ab2957e39c548c6bf71a789004eb44b0da9f4
HENEMAN <laurent.heneman@yahoo.fr>
2025-02-12 15:25:30 +0100

    feat: composant parent-enfant (#15)
    Co-authored-by: Marylinelesaffre <marylineetnoe@outlook.fr>

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,12 @@
 import './App.css'
 
-// import NewButton from './components/new-button'
-// import MyButton from './components/test-julie/my-button'
-// import MyButton1 from './components/my-button1'
-// import MyButton2 from './components/my-button2'
-// import MyButton3 from './components/my-button3'
+import ComposantParent from './components/exo-maryline/composant-parent'
 import Child from './components/test-julie/parent'
 
 function App() {
   return (
     <>
-      {/* <MyButton /> */}
-      {/* <MyButton1 /> */}
-      {/* <MyButton2 /> */}
-      {/* <MyButton3 /> */}
-      {/* <NewButton /> */}
+      <ComposantParent />
       <Child />
     </>
   )
--- src/components/exo-maryline/new-button.tsx
+++ src/components/exo-maryline/new-button.tsx
@@ -3,13 +3,16 @@ import { useState, useEffect, useRef } from 'react'
 function ButtonSquare() {
   const [value, setValue] = useState('')
   const ref = useRef(0)
+  console.log('je suis après ma déclaration de mon useRef')
 
   function handleClick() {
     setValue('X')
     ref.current = ref.current + 1
     console.log('Le bouton à était cliqué ' + ref.current + ' fois !')
   }
+
   useEffect(() => {
+    console.log('je suis dans ma condition useEffect')
     if (value !== '') {
       console.log(`Le bouton contient : ${value}`)
     }
@@ -17,8 +20,9 @@ function ButtonSquare() {
 
   return <button onClick={handleClick}> {value} </button>
 }
-
+console.log('je suis après ma condition')
 export default function NewButton() {
+  console.log('composant NewButton')
   return (
     <>
       <ButtonSquare />
