b8e392f63182383247e970c1ffd458d81073f952
Julie-Charles16 <juliecharles2716@gmail.com>
2025-02-13 11:43:27 +0100

    feat: ajout des props CardChild sur les CardLearning

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import './App.css'
 
 // import ComposantParent from './components/exo-maryline/composant-parent'
 // import Child from './components/test-julie/parent'
-import CardTravel from './components/card-learning'
+import CardChild from './components/card-learning'
 
 function App() {
   return (
     <>
       {/* <ComposantParent /> */}
       {/* <Child /> */}
-      <CardTravel />
+      <CardChild />
     </>
   )
 }
