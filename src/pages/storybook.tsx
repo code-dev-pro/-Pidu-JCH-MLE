68b9d390f0c8c305e3842d807cfe44ec5292142a
Marylinelesaffre <marylineetnoe@outlook.fr>
2025-02-19 16:40:39 +0100

    feat: création du composant image

---
+++ src/pages/storybook.tsx
@@ -0,0 +1,10 @@
+import Picture from '@/components/picture'
+
+export default function StoryBook() {
+  return (
+    <>
+      <h1>Nos composants Pidu</h1>
+      <Picture src="./Happy.svg" alt="image pidu content" />
+    </>
+  )
+}
