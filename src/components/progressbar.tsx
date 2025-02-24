c6987e34a8dbd3e43be9880430c55d7c43d62e64
Marylinelesaffre <marylineetnoe@outlook.fr>
2025-02-24 09:48:47 +0100

    feat: composant unitaire bar de progression

---
+++ src/components/progressbar.tsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+
+export default function Progressbar() {
+  const [progress, setprogress] = useState(0)
+  const count = () => {
+    if (progress + 20 > 100) return
+    setprogress(progress + 20)
+  }
+
+  return (
+    <>
+      <div className="w-full max-w-[895px] bg-gray-200 rounded-full h-[16px] overflow-hidden">
+        <div
+          className="bg-[#FF8B2D] rounded-full h-[16px] transition-[width] duration-300"
+          style={{ width: `${progress}%` }}
+        />
+      </div>
+      <button onClick={count}>ingrementer</button>
+    </>
+  )
+}
--- src/pages/storybook.tsx
+++ src/pages/storybook.tsx
@@ -1,10 +1,12 @@
 import Picture from '@/components/picture'
+import Progressbar from '@/components/progressbar'
 
 export default function StoryBook() {
   return (
     <>
       <h1>Nos composants Pidu</h1>
       <Picture src="./Happy.svg" alt="image pidu content" />
+      <Progressbar />
     </>
   )
 }
