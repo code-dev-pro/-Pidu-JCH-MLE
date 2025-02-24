26c468e5d9262871b9b52dc27a38f4f22d5fc8c9
Julie-Charles16 <juliecharles2716@gmail.com>
2025-02-24 09:50:25 +0100

    feat: création d'un composant score

--- src/pages/storybook.tsx
+++ src/pages/storybook.tsx
@@ -2,6 +2,7 @@ import Title from '@/components/title'
 import CardAnswer from '@/components/card-answer'
 import Picture from '@/components/picture'
 import CardReward from '@/components/card-reward'
+import Number from '@/components/score'
 
 export default function StoryBook() {
   return (
@@ -14,6 +15,7 @@ export default function StoryBook() {
       <CardAnswer index={3} title="Apples" />
       <CardAnswer index={4} title="Coconut" />
       <CardReward />
+      <Number value={1} />
     </>
   )
 }
