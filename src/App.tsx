da8bfc78e20775b81b47772e86abc7b1de71fdbd
Julie-Charles16 <juliecharles2716@gmail.com>
2025-02-26 15:06:16 +0100

    fix: alias2

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
-import Exercice from '@/pages/exercice'
-import Home from '@/pages/home'
-import Level from '@/pages/level'
-import Result from '@/pages/result'
+import Exercice from '@/pages/Exercice'
+import Home from '@/pages/Home'
+import Level from '@/pages/Level'
+import Result from '@/pages/Result'
 import StoryBook from '@/pages/storybook'
 
 function App() {
