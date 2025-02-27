ff5a7ab57674d4b22e260f755945940fe299830d
Julie-Charles16 <juliecharles2716@gmail.com>
2025-02-27 15:35:29 +0100

    fix: border-radius sur card-reward remis après suppression pour test

--- src/components/card-reward.tsx
+++ src/components/card-reward.tsx
@@ -8,7 +8,7 @@ export default function CardReward() {
     score >= 1 && score < 5 ? './Star.svg' : score <= 5 ? './Diamond.svg' : './default.svg/'
 
   return (
-    <div className="bg-[#FBF4DC] rounded- w-[308px] h-[73px] flex items-center justify-center">
+    <div className="bg-[#FBF4DC] rounded-[13px] w-[308px] h-[73px] flex items-center justify-center">
       <span className="text-center font-semibold flex items-center gap-1">
         Tu as gagné <Score value={score} />
         <Picture src={pictureSrc} alt="img récompense" className="w-[24px] h-[20px]" />
