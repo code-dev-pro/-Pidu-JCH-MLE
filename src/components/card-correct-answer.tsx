ffbf683518399b65a6e79d5cbd77283b6e74b3ba
Marylinelesaffre <marylineetnoe@outlook.fr>
2025-02-27 16:59:43 +0100

    feat: ajout de props pour les coulur et la taille des progressNumber

--- src/components/card-correct-answer.tsx
+++ src/components/card-correct-answer.tsx
@@ -6,7 +6,11 @@ export default function ContentCorrectAnswer() {
       <div className="rounded-xl w-64 h-25 flex items-center justify-center p-4 border">
         <div className="flex flex-col font-bold">
           <span className="text-[#646A69] text-[24px]">Réponses correctes</span>
-          <ProgressNumber />
+          <ProgressNumber
+            className="text-4xl font-bold"
+            colorNumber="text-green-500"
+            currentColor="text-green-500"
+          />
         </div>
       </div>
     </>
