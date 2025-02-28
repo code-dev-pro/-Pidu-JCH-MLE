22fb7bbf2127a8aa1e5f1c08afb5bdb1ad3437eb
Julie-Charles16 <juliecharles2716@gmail.com>
2025-02-28 14:23:40 +0100

    feat: création du feedback(1)

--- src/components/feedback.tsx
+++ src/components/feedback.tsx
@@ -1,7 +1,6 @@
 import Picture from './picture'
 import Title from './title'
 import CustomButton from './buttonlong'
-
 interface FeedbackProps {
   isCorrect: boolean
   correctAnswer: string
@@ -9,31 +8,25 @@ interface FeedbackProps {
 }
 
 function Feedback({ isCorrect, correctAnswer, onClick }: FeedbackProps) {
+  const backgroundColor = isCorrect ? '#DFF8EC' : '#FDF3F2'
+  const pictureSrc = isCorrect ? './Happy.svg' : './Sad.svg'
+  const pictureAlt = isCorrect ? 'Bonne réponse' : 'Mauvaise réponse'
+  const title = isCorrect ? 'Bravo !' : 'Oh, non !'
+  const subtitle = isCorrect
+    ? 'Tu as trouvé la bonne réponse!'
+    : `La bonne réponse est : "${correctAnswer}"`
+
   return (
-    <div
-      className="flex items-center w-full h-[144px]"
-      style={{ backgroundColor: isCorrect ? '#DFF8EC' : '#FDF3F2' }}
-    >
+    <div className="flex items-center w-full h-[144px]" style={{ backgroundColor }}>
       <div className="flex flex-row justify-between w-full">
         <div className="flex flex-row ml-8">
-          <Picture
-            src={isCorrect ? './Happy.svg' : './Sad.svg'}
-            alt={isCorrect ? 'Bonne réponse' : 'Mauvaise réponse'}
-            className="mx-auto mb-4 w-24 h-24"
-          />
+          <Picture src={pictureSrc} alt={pictureAlt} className="mx-auto mb-4 w-24 h-24" />
 
           <div className="flex flex-col justify-center ml-4">
             <div className="font-bold mb-2">
-              <Title tag="h2" title={isCorrect ? 'Bravo !' : 'Oh, non !'} />
+              <Title tag="h2" title={title} />
             </div>
-            <Title
-              tag="h3"
-              title={
-                isCorrect
-                  ? 'Tu as trouvé la bonne réponse!'
-                  : `La bonne réponse est : "${correctAnswer}"`
-              }
-            />
+            <Title tag="h3" title={subtitle} />
           </div>
         </div>
 
