aaeae4c0c979178102b9339c35b54b9f870da07f
Julie Charles <126774803+Julie-Charles16@users.noreply.github.com>
2025-03-07 15:07:22 +0100

    feat: création de la page chemin sans boutons (#68)

--- src/pages/Level.tsx
+++ src/pages/Level.tsx
@@ -1,7 +1,23 @@
+import Avatar from '@/components/card-avatar'
+import CardRewardLevel from '@/components/card-reward-level'
+// import ButtonLocked from '@/components/button-cercle'
+
 export default function Level() {
   return (
     <>
-      <h1>Chemin des niveaux</h1>
+      <img src="/Bg-L.jpg" alt="background" className="absolute inset-0 w-full h-full" />
+      <div className="absolute top-0 left-0 flex flex-row p-4">
+        <div className="relative p-2">
+          <Avatar />
+        </div>
+        <div className="flex flex-row gap-7 p-7">
+          <CardRewardLevel />
+          <CardRewardLevel />
+        </div>
+      </div>
+      {/* <div className="relative">
+        <ButtonLocked />
+      </div> */}
     </>
   )
 }
