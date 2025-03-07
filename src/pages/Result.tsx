314de18b2d7a28b0bb334fc52b3272148068921f
Marylinelesaffre <126774712+Marylinelesaffre@users.noreply.github.com>
2025-03-07 09:52:12 +0100

    feat: mise en page de la page bilan (#64)

--- src/pages/Result.tsx
+++ src/pages/Result.tsx
@@ -1,7 +1,19 @@
+import Title from '@/components/title'
+import ContentCorrectAnswer from '@/components/card-correct-answer'
+import CardReward from '@/components/card-reward'
+import CustomButton from '@/components/buttonlong'
 export default function Result() {
   return (
     <>
-      <h1>Bilan du niveau</h1>
+      <div className="w-full h-[250px] bg-[url(./Background-Bilan.svg)] bg-cover"></div>
+      <Title tag="h1" title="Bilan exercice" className="p-5" />
+      <div className="flex flex-row items-center justify-center gap-7 mt-5">
+        <ContentCorrectAnswer />
+        <CardReward />
+      </div>
+      <div className="flex justify-center gap-12 mt-7">
+        <CustomButton text="Rejouer" />
+      </div>
     </>
   )
 }
