365cb2603795ea48d76586f22adede03bf98d6b1
Julie-Charles16 <juliecharles2716@gmail.com>
2025-03-07 10:04:31 +0100

    feat: création de la page Home1

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,15 @@
-import Layout from '@/components/layout/layout'
 import Title from '@/components/title'
 import CustomButton from '@/components/buttonlong'
 
 export default function Home() {
   return (
-    <Layout>
+    <>
       <img src="./Bg-Home.jpg" className="h-[422px]"></img>
       <Title title="Bienvenue sur PIDU" tag="h1" className="mt-16 text-[#646A69]" />
       <Title title="Pour apprendre l'anglais en s'amusant" tag="h2" className="mt-12 text-xl" />
       <div className="flex justify-center mt-14 text-xl">
         <CustomButton bgColor="green" text="Démarrer" />
       </div>
-    </Layout>
+    </>
   )
 }
