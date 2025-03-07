7451fb63818360a5a7cfad6abc7111543800db7b
Julie-Charles16 <juliecharles2716@gmail.com>
2025-03-07 09:55:56 +0100

    feat: création de la page Home

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,20 @@ import Level from '@/pages/Level'
 import Result from '@/pages/Result'
 import Exercice from '@/pages/Exercice'
 import StoryBook from '@/pages/storybook'
+import Layout from './components/layout/layout'
 
 function App() {
   return (
-    <Routes>
-      <Route path="" element={<Home />} />
-      <Route path="level" element={<Level />} />
-      <Route path="exercice/:levelId/:questionId" element={<Exercice />} />
-      <Route path="exercice" element={<Exercice />} />
-      <Route path="result" element={<Result />} />
-      <Route path="storybook" element={<StoryBook />} />
-    </Routes>
+    <Layout>
+      <Routes>
+        <Route path="" element={<Home />} />
+        <Route path="level" element={<Level />} />
+        <Route path="exercice/:levelId/:questionId" element={<Exercice />} />
+        <Route path="exercice" element={<Exercice />} />
+        <Route path="result" element={<Result />} />
+        <Route path="storybook" element={<StoryBook />} />
+      </Routes>
+    </Layout>
   )
 }
 
