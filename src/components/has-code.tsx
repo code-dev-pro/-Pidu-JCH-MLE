e0d6b888ce4da9b3f5d658328a065b6e2dad686b
Julie Charles <126774803+Julie-Charles16@users.noreply.github.com>
2025-04-07 14:32:03 +0200

    refactor: refactorisation de la page home (#85)

---
+++ src/components/has-code.tsx
@@ -0,0 +1,130 @@
+import { useRef, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import authenticateCode from '@/services/service-auth'
+import useAuthStore from '@/store/tracking/tracker-auth'
+import useLevelStore from '@/store/store-level'
+import useProgressStore from '@/store/tracking/tracker-progress'
+import useTotalQuizStore from '@/store/tracking/tracker-total-answer'
+import { API_URL } from '@/config/api'
+
+interface ExerciseData {
+  exercise_id: number
+  question_id: number
+  iscorrect: boolean
+}
+
+export const HasCode = () => {
+  const inputRef = useRef<HTMLInputElement | null>(null)
+  const [message, setMessage] = useState('')
+  const [isLoading, setIsLoading] = useState(false)
+  const { setUserId } = useAuthStore()
+  const { setLevel } = useLevelStore()
+  const { setProgress } = useProgressStore()
+  const { addTotalAnswer } = useTotalQuizStore()
+  const navigate = useNavigate()
+
+  const handleAuthCode = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const userCode = inputRef.current?.value
+    if (!userCode) {
+      setMessage('Le code ne peut pas être vide.')
+      return
+    }
+
+    setIsLoading(true)
+
+    const fetchUserData = async (userId: number) => {
+      try {
+        const response = await fetch(`${API_URL}/level/${userId}`)
+        const data = await response.json()
+
+        if (data && data.length > 0) {
+          const sortedData = data.sort(
+            (a: ExerciseData, b: ExerciseData) =>
+              b.exercise_id - a.exercise_id || b.question_id - a.question_id
+          )
+
+          const { exercise_id, question_id } = sortedData[0]
+
+          if (question_id >= 5) {
+            setLevel(exercise_id + 1)
+            setProgress(1)
+          } else {
+            setLevel(exercise_id)
+            setProgress(question_id + 1)
+          }
+          addTotalAnswer(data)
+        } else {
+          console.log('Pas de données disponibles')
+        }
+      } catch (error) {
+        console.error('Erreur lors de la récupération des données utilisateur:', error)
+      }
+    }
+
+    try {
+      const response = await authenticateCode(userCode)
+      console.log("Réponse de l'API :", response)
+      if (response.success && typeof response.userId === 'number' && response.userId > 0) {
+        setUserId(response.userId)
+        await fetchUserData(response.userId)
+        setMessage('Code valide ✅')
+
+        // Attendre que l'utilisateur voie le message avant de naviguer
+        setTimeout(() => {
+          setIsLoading(false) // Désactiver le chargement
+          navigate('/Level')
+        }, 2000)
+      } else {
+        setMessage('Code invalide ❌')
+
+        // Attendre un peu avant de réinitialiser le formulaire
+        setTimeout(() => {
+          if (inputRef.current) {
+            inputRef.current.value = ''
+          }
+          setMessage('')
+          setIsLoading(false) // Désactiver le chargement après réinitialisation
+        }, 2000)
+      }
+    } catch (error) {
+      console.error('Erreur serveur:', error)
+      setMessage('Erreur de connexion au serveur ❌')
+
+      // Attendre un peu avant de réinitialiser le formulaire
+      setTimeout(() => {
+        if (inputRef.current) {
+          inputRef.current.value = ''
+        }
+        setMessage('')
+        setIsLoading(false) // Désactiver le chargement après réinitialisation
+      }, 2000)
+    }
+  }
+
+  return (
+    <div className="">
+      <div className="flex flex-col mt-2">
+        <h2 className="text-xl font-semibold text-black">Entrez votre code</h2>
+        <form onSubmit={handleAuthCode} className="flex flex-col items-center space-y-3">
+          <input
+            type="text"
+            placeholder="Code utilisateur"
+            ref={inputRef}
+            required
+            className="border p-2 rounded-md"
+          />
+          <button
+            type="submit"
+            className="px-4 py-2 rounded-4xl bg-[#19C472] text-white cursor-pointer border-b-6 border-black/20 w-40 
+            hover:scale-110 transition-transform duration-300"
+            disabled={isLoading} // Désactiver le bouton pendant le chargement
+          >
+            {isLoading ? 'Chargement...' : 'Valider'}
+          </button>
+        </form>
+        {message && <p className="mt-2">{message}</p>}
+      </div>
+    </div>
+  )
+}
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,98 +1,27 @@
 import Title from '@/components/title'
 import CustomButton from '@/components/buttonlong'
-import { useNavigate } from 'react-router-dom'
 import useExerciseStore from '@/store/data/exercise'
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { CodeGenerator } from '@/components/code-generator'
-import authenticateCode from '@/services/service-auth'
-import useAuthStore from '@/store/tracking/tracker-auth'
-import useLevelStore from '@/store/store-level'
-import useProgressStore from '@/store/tracking/tracker-progress'
-import useTotalQuizStore from '@/store/tracking/tracker-total-answer'
-import { API_URL } from '@/config/api'
-
-interface ExerciseData {
-  exercise_id: number
-  question_id: number
-  iscorrect: boolean
-}
+import { HasCode } from '@/components/has-code'
 
 export default function Home() {
-  const { setUserId } = useAuthStore()
-  const { setLevel } = useLevelStore()
-  const { setProgress } = useProgressStore()
-  const { addTotalAnswer } = useTotalQuizStore()
-  const { exercises, isLoading, error, loadExercises } = useExerciseStore()
-
-  const inputRef = useRef<HTMLInputElement | null>(null)
-  const [message, setMessage] = useState('')
-  const navigate = useNavigate()
+  const { isLoading, error, loadExercises } = useExerciseStore()
+  const [showCodeGenerator, setShowCodeGenerator] = useState(false)
+  const [showHasCode, setShowHasCode] = useState(false)
 
   useEffect(() => {
-    console.log('Chargement des exercices...')
     loadExercises()
   }, [loadExercises])
 
-  const fetchUserData = async (userId: number) => {
-    try {
-      const response = await fetch(`${API_URL}/level/${userId}`)
-      const data = await response.json()
-
-      console.log('Données utilisateur récupérées :', data)
-
-      if (data && data.length > 0) {
-        const sortedData = data.sort(
-          (a: ExerciseData, b: ExerciseData) =>
-            b.exercise_id - a.exercise_id || b.question_id - a.question_id
-        )
-
-        const { exercise_id, question_id } = sortedData[0]
-
-        console.log(`Exercice le plus avancé : ${exercise_id}, Dernière question : ${question_id}`)
-
-        if (question_id >= 5) {
-          setLevel(exercise_id + 1)
-          setProgress(1)
-        } else {
-          setLevel(exercise_id)
-          setProgress(question_id + 1)
-        }
-        addTotalAnswer(data)
-      } else {
-        console.log('Pas de données disponibles')
-      }
-    } catch (error) {
-      console.error('Erreur lors de la récupération des données utilisateur:', error)
-    }
+  const handleStart = () => {
+    setShowCodeGenerator(true) // Affiche CodeGenerator
+    setShowHasCode(false) // Cache HasCode au cas où il était affiché
   }
 
-  const handleAuthCode = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    const userCode = inputRef.current?.value
-    if (!userCode) {
-      setMessage('Le code ne peut pas être vide.')
-      return
-    }
-
-    try {
-      const response = await authenticateCode(userCode)
-      console.log("Réponse de l'API :", response)
-      if (response.success && typeof response.userId === 'number' && response.userId > 0) {
-        setUserId(response.userId)
-        await fetchUserData(response.userId)
-        setMessage('Code valide ✅')
-        navigate('/Level')
-      } else {
-        setMessage('Code invalide ❌')
-      }
-    } catch (error) {
-      console.error('Erreur serveur:', error)
-      setMessage('Erreur de connexion au serveur ❌')
-    }
-  }
-
-  const handleClick = () => {
-    navigate('/Level')
+  const handleShowHasCode = () => {
+    setShowHasCode(true) // Affiche HasCode
+    setShowCodeGenerator(false) // Cache CodeGenerator au cas où
   }
 
   return (
@@ -106,34 +35,29 @@ export default function Home() {
       <Title
         title="Pour apprendre l'anglais en s'amusant"
         tag="h2"
-        className="mt-3 text-lg md:text-xl lg:text-2xl text-center"
+        className="mt-3 text-lg text-black md:text-xl lg:text-2xl text-center"
       />
 
       {isLoading && <p>Chargement...</p>}
-      {error && <p className="text-red-500">{error}</p>}
-      {!isLoading && !error && exercises.length === 0 && <p>Aucun exercice disponible.</p>}
 
-      <div className="flex justify-center mt-6">
-        <CustomButton bgColor="green" text="Démarrer" onClickHandler={handleClick} />
-      </div>
-      <CodeGenerator />
-
-      <div className="flex flex-col items-center mt-6">
-        <h2 className="text-xl font-semibold">Entrez votre code</h2>
-        <form onSubmit={handleAuthCode} className="flex flex-col items-center space-y-3">
-          <input
-            type="text"
-            placeholder="Code utilisateur"
-            ref={inputRef}
-            required
-            className="border p-2 rounded-md"
-          />
-          <button type="submit" className="px-4 py-2 bg-green-500 text-white rounded-md">
-            Valider
-          </button>
-        </form>
-        {message && <p className="mt-2">{message}</p>}
-      </div>
+      {error ? (
+        <p className="text-red-500">{error}</p>
+      ) : (
+        <>
+          <div className="flex flex-col items-center gap-4 mt-6">
+            <div className="flex gap-6">
+              <CustomButton bgColor="green" text="Je veux un code" onClickHandler={handleStart} />
+              <CustomButton
+                bgColor="green"
+                text="J'ai un code"
+                onClickHandler={handleShowHasCode}
+              />
+            </div>
+            {showCodeGenerator && <CodeGenerator />}
+            {showHasCode && <HasCode />}
+          </div>
+        </>
+      )}
     </>
   )
 }
