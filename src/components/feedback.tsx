b5d6b7f79d2436a3ad755ef3ce15eab0773d6619
Marylinelesaffre <126774712+Marylinelesaffre@users.noreply.github.com>
2025-04-10 13:33:20 +0200

    feat: animations pidu sur le feedback (#99)

--- src/components/feedback.tsx
+++ src/components/feedback.tsx
@@ -1,9 +1,9 @@
-import Picture from './picture'
 import Title from './title'
 import CustomButton from './button-long'
 import useFeedbackStore from '@/store/data/feedback'
 import useSound from 'use-sound'
 import { useEffect } from 'react'
+import AnimationHappy from './animation-happy'
 
 interface FeedbackProps {
   correctAnswer: string
@@ -14,8 +14,8 @@ function Feedback({ correctAnswer, increaseProgress }: FeedbackProps) {
   const { valueFeedback } = useFeedbackStore()
   const isSuccess = valueFeedback === 'success'
   const backgroundColor = isSuccess ? '#DFF8EC' : '#FDF3F2'
-  const pictureSrc = isSuccess ? './Happy.svg' : './Sad.svg'
-  const pictureAlt = isSuccess ? 'Bonne réponse' : 'Mauvaise réponse'
+  const animationSrc = isSuccess ? './animations/pidu-happy-bg.riv' : './animations/pidu-sad-bg.riv'
+  const animationAlt = isSuccess ? 'Bonne réponse' : 'Mauvaise réponse'
   const title = isSuccess ? 'Bravo !' : 'Oh, non !'
   const subtitle = isSuccess
     ? 'Tu as trouvé la bonne réponse!'
@@ -34,7 +34,11 @@ function Feedback({ correctAnswer, increaseProgress }: FeedbackProps) {
     >
       <div className="flex flex-col sm:flex-row justify-between items-center w-full">
         <div className="flex flex-col sm:flex-row items-center sm:ml-8">
-          <Picture src={pictureSrc} alt={pictureAlt} className="mx-auto w-24 h-24 mb-4 sm:mb-0" />
+          <AnimationHappy
+            src={animationSrc}
+            alt={animationAlt}
+            className="mx-auto w-24 h-24 mb-4 sm:mb-0"
+          />
           <div className="flex flex-col justify-center sm:ml-4 text-center sm:text-left">
             <div className="font-bold mb-2">
               <Title tag="h2" title={title} className={color} />
