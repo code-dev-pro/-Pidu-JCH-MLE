63a289beb58c125ba117cbc5d6bd7ef8aa962642
Julie Charles <126774803+Julie-Charles16@users.noreply.github.com>
2025-04-15 09:08:24 +0200

    feat: ajout du code-user sur la page level (#103)

--- src/services/service-auth.ts
+++ src/services/service-auth.ts
@@ -1,4 +1,5 @@
 import { API_URL } from '@/config/api'
+import useAuthStore from '@/store/tracking/tracker-auth'
 
 const authenticateCode = async (code_user: string) => {
   try {
@@ -10,10 +11,21 @@ const authenticateCode = async (code_user: string) => {
       body: JSON.stringify({ code_user }),
     })
 
-    return await response.json()
+    const data = await response.json()
+
+    if (response.ok && data.success) {
+      // Si l'authentification est réussie, stocke l'userId et le code_user dans le store
+      useAuthStore.getState().setUserId(data.userId)
+      useAuthStore.getState().setCodeUser(code_user) // Stocke également le code utilisateur
+
+      return data
+    } else {
+      return { success: false, message: 'Code invalide.' }
+    }
   } catch (error) {
     console.error('Erreur serveur:', error)
     return { success: false, message: 'Erreur serveur' }
   }
 }
+
 export default authenticateCode
--- src/store/tracking/tracker-auth.tsx
+++ src/store/tracking/tracker-auth.tsx
@@ -2,12 +2,16 @@ import { create } from 'zustand'
 
 interface AuthState {
   userId: number | null
+  codeUser: string | null
   setUserId: (id: number) => void
+  setCodeUser: (code: string) => void
 }
 
 const useAuthStore = create<AuthState>(set => ({
   userId: null,
+  codeUser: null, // Ajoute une propriété pour stocker le code utilisateur
   setUserId: id => set({ userId: id }),
+  setCodeUser: code => set({ codeUser: code }), // Ajoute une méthode pour définir le code utilisateur
 }))
 
 export default useAuthStore
